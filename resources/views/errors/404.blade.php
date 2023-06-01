b074023ee8de9a3caa86771c9a41c099b7b66d9d
Kriikle <kirill@mail.ru>
2023-06-02 00:08:29 +0300

    Adding 404 error blade

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <section id="home-section" class="hero">
+        <h1 class="mb-0 bread" style="text-align: center;padding-bottom: 200px">Page not found: 404</h1>
+    </section>
+@endsection
